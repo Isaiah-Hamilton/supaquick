21f7091cd8f2c3aee9f5164aacd6a4214ace39ec
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-01-06 18:03:37 -0500

    create project directory

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,69 @@
 mod ui;
+mod utils;
 
 use ui::{input, list, logo, option};
+use utils::directory::{create_directory, directory_exists};
 
 fn main() {
     logo();
-    let import_options = ["Automatic", "Manual"];
-    let frameworks = [
-        "Next.js",
-        "React",
-        "NuxtJS",
-        "RedwoodJS",
-        "Flutter",
-        "Android Kotlin",
-        "SvelteKit",
-        "SolidJS",
-        "Vue",
-        "refine",
-    ];
 
-    let name = input("What is your project named: ", "my-app", "my-app");
-    let framework = list("Choose a framework:", &frameworks);
-    let framework_template = list("Choose a template:", &frameworks);
-    let import_option = option(
-        "Import Supabase project automatically or manually: ",
-        &import_options,
-    );
+    let project_name = input("What is your project named: ", "my-app", "my-app");
 
-    if import_option == import_options[0] {
-        automatic_import();
+    if directory_exists(&project_name) {
+        println!("{} already exists", project_name);
     } else {
-        let project_url = input("Project url: ", "", "");
-        let project_anon_key = input("What is your Project anon key: ", "", "");
+        create_directory(&project_name);
     }
-}
 
-fn automatic_import() {
-    let organizations = ["supabase", "Isaiah-Hamilton"];
-    let options = ["New", "Existing"];
-    let existing_projects = ["test", "test2"];
-    let regions = ["north america", "south america"];
+    /*
+        let import_options = ["Automatic", "Manual"];
+        let frameworks = [
+            "Next.js",
+            "React",
+            "NuxtJS",
+            "RedwoodJS",
+            "Flutter",
+            "Android Kotlin",
+            "SvelteKit",
+            "SolidJS",
+            "Vue",
+            "refine",
+        ];
 
-    let access_token = input("access token: ", "", "");
-    let organization = list("Choose an organization:", &organizations);
-    let new_project = option(
-        "Create new Supabase project or use existing project: ",
-        &options,
-    );
+        let framework = list("Choose a framework:", &frameworks);
+        let framework_template = list("Choose a template:", &frameworks);
+        let import_option = option(
+            "Import Supabase project automatically or manually: ",
+            &import_options,
+        );
 
-    if new_project == options[0] {
-        let project_name = input("What is your project named: ", "", "");
-        let database_password = input("What is your database password: ", "", "");
-        let project_region = list("Where is your project location: ", &regions);
-    } else {
-        let project = list("Choose an existing porject:", &existing_projects);
+        if import_option == import_options[0] {
+            automatic_import();
+        } else {
+            let project_url = input("Project url: ", "", "");
+            let project_anon_key = input("What is your Project anon key: ", "", "");
+        }
     }
+
+    fn automatic_import() {
+        let organizations = ["supabase", "Isaiah-Hamilton"];
+        let options = ["New", "Existing"];
+        let existing_projects = ["test", "test2"];
+        let regions = ["north america", "south america"];
+
+        let access_token = input("access token: ", "", "");
+        let organization = list("Choose an organization:", &organizations);
+        let new_project = option(
+            "Create new Supabase project or use existing project: ",
+            &options,
+        );
+
+        if new_project == options[0] {
+            let project_name = input("What is your project named: ", "", "");
+            let database_password = input("What is your database password: ", "", "");
+            let project_region = list("Where is your project location: ", &regions);
+        } else {
+            let project = list("Choose an existing porject:", &existing_projects);
+        }
+        */
 }
